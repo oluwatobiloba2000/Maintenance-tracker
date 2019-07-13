cbbe81a398932757e303abb735fbf8c55be08957
Anani oluwatobiloba <ananioluwatobiloba2000@gmail.com>
2019-07-13 20:06:12 +0100

    added controllers

--- api/v1/src/controllers/index.js
+++ api/v1/src/controllers/index.js
@@ -1,48 +1,58 @@
-const uuidv1 = require("uuid/v1");
-let requests = require("../data/data");
+const uuidv1 = require('uuid/v1');
 
-class  RequestController {
+const requests = require("../data/data");
+
+class RequestController {
     //GET /requests getting all requesta
-    static allRequests(req ,res){
+    static allRequests(req, res) {
         return res.json({
+            "Total requests": requests.length,
             requests
         });
     }
 
     //GET /requests/:<request id>
-    static getRequestById(req, res){
+    static getRequestById(req, res) {
         const id = req.params.id;
-        const request = requests.find(each => each.id == id);
-        if(!request){
+        const request = requests.find(each => each.id === id);
+        if (!request) {
             return res.status(404).json({
-                message : `request with id:${id} cannot be found , check the id and try again !`
+                message: `request with id:${id} cannot be found , check the id and try again !`
             });
         }
-        return res.json({ request });
+        return res.json({
+            request
+        });
     }
 
     //POST /request
-    static createRequest(req ,res){
+    static createRequest(req, res) {
         const title = req.body.title;
         const description = req.body.description;
-        if(!name || !description){
+        if (!title || !description) {
             return res.status(400).json({
-                message : `request must must have a title and description`
+                message: `request must have a title and description`
             });
         }
         const id = uuidv1();
 
         //create request
         const request = {
-            id : id,
-            title : title,
-            description : description,
-            time_requested : new Date(),
-            status : pending
+            id: id,
+            title: title,
+            description: description,
+            time_requested: new Date(),
+            status: 'pending'
         }
 
         //append it to the request array
         requests.push(request);
-        return res.json({request});
+        return res.json({
+            message: `Request made successfully`,
+
+            request
+        });
     }
 }
+
+module.exports = RequestController;
