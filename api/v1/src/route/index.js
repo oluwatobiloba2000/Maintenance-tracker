25215fa792af01e47a6e5f8f6a4e4c5f2bffdedf
Anani oluwatobiloba <ananioluwatobiloba2000@gmail.com>
2019-07-14 00:44:17 +0100

    put request to modify request made

--- api/v1/src/route/index.js
+++ api/v1/src/route/index.js
@@ -2,16 +2,21 @@ let express = require("express");
 const controller = require('../controllers')
 //initialize express
 const app = express();
-
+//GET homepage
 app.get('/' , (req ,res) =>{
     return res.json({message : `Maintenance Tracker Api version 1`});
 });
-
+//GET get all requests
 app.get('/request' , controller.allRequests);
 
-app.get('/request/:id' , controller.getRequestById)
+//GET get a request by its id 
+app.get('/request/:id' , controller.getRequestById);
 
+//POST create a request
 app.post('/request' , controller.createRequest);
+
+//PUT modify a request by id
+app.put('/request/:id' , controller.updateRequest);
 
 app.use('*' , (req, res)=>{
     return res.send('Route not found');
